5047d96f4c124d73814fa088e97950dca4b99480
Miika Kontio <miika.kontio@prime.fi>
2022-06-07 12:17:13 +0300

    MS unit tests

---
+++ FileSplicerLib.Tests/FileSplicerLibTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileSplicerLib.Tests {
+
+  [TestClass]
+  public class CalculateHowManyFilesToCreateTests {
+
+    [TestMethod]
+    public void CalculateHowManyFilesToCreate_ZeroLinesPerFile_Test() {
+      var linesPerFile = 0;
+      var splicer = new FileSplicer(linesPerFile, "");
+      var fileLines = GetFileLines();
+      var res = splicer.CalculateHowManyFilesToCreate(fileLines);
+      Assert.AreEqual(0, res);
+    }
+
+    [TestMethod]
+    public void CalculateHowManyFilesToCreate_1LinesPerFile_Test() {
+      var linesPerFile = 1;
+      var splicer = new FileSplicer(linesPerFile, "");
+      var fileLines = GetFileLines();
+      var res = splicer.CalculateHowManyFilesToCreate(fileLines);
+      Assert.AreEqual(3, res);
+    }
+
+    [TestMethod]
+    public void CalculateHowManyFilesToCreate_2LinesPerFile_Test() {
+      var linesPerFile = 2;
+      var splicer = new FileSplicer(linesPerFile, "");
+      var fileLines = GetFileLines();
+      var res = splicer.CalculateHowManyFilesToCreate(fileLines);
+      Assert.AreEqual(2, res);
+    }
+
+    [TestMethod]
+    public void CalculateHowManyFilesToCreate_3LinesPerFile_Test() {
+      var linesPerFile = 3;
+      var splicer = new FileSplicer(linesPerFile, "");
+      var fileLines = GetFileLines();
+      var res = splicer.CalculateHowManyFilesToCreate(fileLines);
+      Assert.AreEqual(1, res);
+    }
+
+    [TestMethod]
+    public void CalculateHowManyFilesToCreate_4LinesPerFile_Test() {
+      var linesPerFile = 4;
+      var splicer = new FileSplicer(linesPerFile, "");
+      var fileLines = GetFileLines();
+      var res = splicer.CalculateHowManyFilesToCreate(fileLines);
+      Assert.AreEqual(1, res);
+    }
+
+    private static string[] GetFileLines() {
+      string[] fileLines = { "row1", "row2", "row3" };
+      return fileLines;
+    }
+
+    [TestMethod]
+    public void CalculateHowManyFilesToCreate_EmptyLines_Test() {
+      var linesPerFile = 250;
+      var splicer = new FileSplicer(linesPerFile, "");
+      string[] fileLines = { };
+      var res = splicer.CalculateHowManyFilesToCreate(fileLines);
+      Assert.AreEqual(0, res);
+    }
+  }
+}
---
+++ FileSplicerLib.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FileSplicerLib.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FileSplicerLib.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59ba045d-924d-45a5-9255-957ebc909394")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
